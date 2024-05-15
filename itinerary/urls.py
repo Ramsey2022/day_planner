1817b714137d0d8cdbf9a2027824d8fa5f876676
Ramsey2022 <abovethelotus@yahoo.com>
2024-05-15 12:05:51 -0700

    created signup/signin forms and linked via urls

---
+++ itinerary/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path, re_path
+from . import views
+
+urlpatterns = [
+    re_path(r"^$", views.index, name="index"),
+    path("login/", views.login_view, name="login"),
+    path("logout/", views.logout_view, name="logout"),
+    path("signup/", views.signup, name="signup"),
+]
--- itinerary/views.py
+++ itinerary/views.py
@@ -1,3 +1,59 @@
 from django.shortcuts import render
+from django.contrib.auth import login, logout
+from django.contrib.auth.models import User
+from django.http import HttpResponseRedirect, HttpResponse
+from django.shortcuts import get_object_or_404
+from django.urls import reverse
+from django.contrib.auth.hashers import make_password
+import requests as req
+
+from itinerary.forms import LoginForm, SignUpForm
+
 
 # Create your views here.
+
+
+def signup(request):
+    if request.method == "POST":
+        username = request.POST.get("username")
+        password = request.POST.get("password")
+        try:
+            user = User.objects.filter(username=username).first()
+            if user:
+                return render(
+                    request,
+                    "signup.html",
+                    {"form": SignUpForm, "message": "user already exists"},
+                )
+            else:
+                user = User.objects.create(
+                    username=username, password=make_password(password)
+                )
+                login(request, user)
+                return HttpResponseRedirect(reverse("index"))
+        except User.DoesNotExist:
+            return render(request, "signup.html", {"form": SignUpForm})
+    return render(request, "signup.html", {"form": SignUpForm})
+
+
+def login_view(request):
+    if request.method == "POST":
+        username = request.POST.get("username")
+        password = request.POST.get("password")
+        try:
+            user = User.objects.get(username=username)
+            if user.check_password(password):
+                login(request, user)
+                return HttpResponseRedirect(reverse("index"))
+        except User.DoesNotExist:
+            return render(request, "login.html", {"form": LoginForm})
+    return render(request, "login.html", {"form": LoginForm})
+
+
+def logout_view(request):
+    logout(request)
+    return HttpResponseRedirect(reverse("login"))
+
+
+def index(request):
+    return render(request, "index.html")
