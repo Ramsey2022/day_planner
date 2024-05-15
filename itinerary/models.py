694b0e7b9b9e752d9e774524000e3d82e02c8e77
Ramsey2022 <abovethelotus@yahoo.com>
2024-05-15 10:11:55 -0700

    updated models

--- itinerary/models.py
+++ itinerary/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+
+class Park(models.Model):
+    name = models.CharField(max_length=255)
+    address = models.CharField(max_length=255)
+    rating = models.IntegerField()
+
+
+class Weather(models.Model):
+    day = models.CharField(max_length=255)
+    date = models.DateField()
+    temp = models.IntegerField()
+    main = models.CharField(max_length=255)
